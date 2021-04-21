192b17504c9142e8a453d3e29b481ed6ba7a8ea9
bagastri07 <bagaskuhaku@gmail.com>
2021-04-22 00:18:34 +0700

    fix heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const env = require('env')
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('Assets'))
