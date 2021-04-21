0f3132dc5b26ed89fb33c6c475749826b5ab1457
bagastri07 <bagaskuhaku@gmail.com>
2021-04-22 00:01:32 +0700

    add new

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const env = require('env')
 
-const port = 3000
+const port = process.env.PORT | 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('Assets'))
 
 app.get('/', (req, res) => {
-    const invitation = req.query.invitation
-    res.send(invitation)
+    const invitation = req.query.invitation;
+    res.render('index', {invitation})
 })
 
 app.listen(port, ()=> {
-    console.log('Listening port:', port)
+    console.log('Listening port:', port);
 })
 
