c6970e284769c53f95341583138f233e7710c213
bagastri07 <bagaskuhaku@gmail.com>
2021-04-22 00:38:27 +0700

    new add

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('assets'))
-app.set('views', __dirname + 'views')
+app.set('views', __dirname + '/views')
 
 app.get('/', (req, res) => {
     const invitation = req.query.invitation;
